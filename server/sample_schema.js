8ec3a6947d28d87180b6174294e07462c716703b
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-12-28 11:46:00 +0530

    Add SysInfo schema as the default

--- server/sample_schema.js
+++ server/sample_schema.js
@@ -6,31 +6,85 @@ if(!Package['kadira:runtime-dev']) {
 const {
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLList
 } = GraphQL.types;
 
-const query = new GraphQLObjectType({
-  name: 'EchoAPI',
-  description: "A simple GraphQL API which has a one field called echo",
+const OS = Npm.require('os');
+
+const LoadAverage = new GraphQLObjectType({
+  name: 'LoadAverage',
+  description: 'Load average of the system.',
   fields: {
-    echo: {
-      description: "Simply echo back the message your are sending",
+    min15: {
+      type: GraphQLFloat,
+      description: "Load average of last 15 minutes."
+    },
+    min5: {
+      type: GraphQLFloat,
+      description: "Load average of last 5 minutes."
+    },
+    min1: {
+      type: GraphQLFloat,
+      description: "Load average of last minute."
+    }
+  }
+});
+
+const SysInfo = new GraphQLObjectType({
+  name: 'SysInfo',
+  description: 'Contains some useful system information.',
+  fields: {
+    hostname: {
       type: GraphQLString,
-      args: {
-        message: {
-          type: GraphQLString,
-          description: "The message to echo"
-        }
-      },
-      resolve(parent, {message}) {
-        return `Echo: ${message}`;
+      description: 'Hostname of the system running the app.',
+      resolve() {
+        return OS.hostname()
+      }
+    },
+    memory: {
+      type: GraphQLFloat,
+      description: 'Memory usage of the app in mega bytes.',
+      resolve() {
+        return process.memoryUsage().rss / 1024 / 1024;
+      }
+    },
+    uptime: {
+      type: GraphQLFloat,
+      description: 'Uptime of the app in seconds.',
+      resolve() {
+        return OS.uptime();
+      }
+    },
+    loadavg: {
+      type: LoadAverage,
+      description: 'Load average of the system running the app.',
+      resolve() {
+        const [min1, min5, min15] = OS.loadavg();
+        return {min15, min5, min1};
       }
     }
   }
 });
 
-const schema = new GraphQLSchema({
+const query = new GraphQLObjectType({
+  name: 'SysInfoQueries',
+  description: "A simple GraphQL API which has a one field called echo.",
+  fields: {
+    sysInfo: {
+      description: "Get basic system information of the app's process.",
+      type: SysInfo,
+      resolve() {
+        // We need to return something
+        return {};
+      }
+    }
+  }
+});
+
+const SysInfoSchema = new GraphQLSchema({
   query
 });
 
-GraphQL.registerSchema('GraphQL Echo', schema);
+GraphQL.registerSchema('SysInfo', SysInfoSchema);
