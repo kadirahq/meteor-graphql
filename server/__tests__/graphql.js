f3ec9b24e1a687d98384600f97062bc2bfb21fb5
Muhammed Thanish <mnmtanish@gmail.com>
2015-12-30 11:51:23 +0530

    Write tests

--- server/__tests__/graphql.js
+++ server/__tests__/graphql.js
@@ -1,3 +1,4 @@
+const Fiber = Npm.require('fibers');
 const graphql = Npm.require('graphql');
 
 describe('Server Side API', () => {
@@ -16,7 +17,7 @@ describe('Server Side API', () => {
 
   describe('GraphQL.registerSchema', () => {
     it('should register a schema', done => {
-      const schema = {aa: Random.id()};
+      const schema = {aa: Random.id(), getTypeMap() { return {}; }};
       const name = Random.id();
 
       GraphQL.registerSchema(name, schema);
@@ -25,13 +26,54 @@ describe('Server Side API', () => {
     });
 
     it('should throw an error for existing schema', done => {
-      const schema = {aa: Random.id()};
+      const schema = {aa: Random.id(), getTypeMap() { return {}; }};
       const name = Random.id();
 
       GraphQL.registerSchema(name, schema);
       const run = () => GraphQL.registerSchema(name, schema);
       expect(run).to.throw(Error);
       done();
+    });
+
+    it('should resolve fields with existing Fiber', done => {
+      const f1 = {resolve() { return {fib: Fiber.current, foo: 'bar'} }};
+      const t1 = {getFields() { return {f1} }};
+      const s1 = {getTypeMap() { return {t1} }};
+
+      const name = Random.id();
+      GraphQL.registerSchema(name, s1);
+
+      const fiber = Fiber.current;
+      expect(fiber).to.not.equal(undefined);
+
+      const out = s1.getTypeMap().t1.getFields().f1.resolve();
+      Promise.resolve(out).then(res => {
+        expect(res.foo).to.equal('bar');
+        expect(res.fib).to.equal(fiber);
+        done();
+      }).catch(done);
+    });
+
+    it('should resolve fields with new Fiber', done => {
+      const f1 = {resolve() { return {fib: Fiber.current, foo: 'bar'} }};
+      const t1 = {getFields() { return {f1} }};
+      const s1 = {getTypeMap() { return {t1} }};
+
+      const name = Random.id();
+      GraphQL.registerSchema(name, s1);
+
+      const fiber = Fiber.current;
+      expect(fiber).to.not.equal(undefined);
+
+      setImmediate(function () {
+        const out = s1.getTypeMap().t1.getFields().f1.resolve();
+        Promise.resolve(out).then(res => {
+          expect(res.foo).to.equal('bar');
+          expect(res.fib).to.not.equal(fiber);
+          expect(res.fib).to.not.equal(undefined);
+          done();
+        }).catch(done);
+      });
     });
   });
 });
