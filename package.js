8e211cb9379791f521130fefb6c18f302fb0ac23
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-12-30 18:39:56 +0530

    v1.2.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'kadira:graphql',
   summary: 'GraphQL support for Meteor with Lokka',
-  version: '1.2.0',
+  version: '1.2.1',
   git: 'https://github.com/kadirahq/meteor-graphql.git'
 });
 
